a3491f45384ab671713e458834d4b30b5b640ac6
fernando <lnfernandobr@gmail.com>
2025-11-19 08:58:06 -0400

    cleaning project

--- app/layout.js
+++ app/layout.js
@@ -1,26 +1,10 @@
 import "./globals.css";
-import { ThemeProvider } from "./components/ThemeProvider";
-import { LanguageProvider } from "./components/LanguageProvider";
-import { Header } from "./components/Header";
 
-export const metadata = {
-  title: "Fernando Lima",
-  description:
-    "I'm Fernando Lima, a Full Stack Software Engineer with experience in React, Tailwind CSS, Node.js, GraphQL, and more. Currently working on micro-SaaS projects and teaching at codeftw.dev.",
-};
-
-export default function RootLayout({ children }) {
+export default function RootLayout() {
   return (
-    <html lang="en" className="h-full dark" style={{ colorScheme: 'dark' }}>
-      <body className="bg-white dark:bg-zinc-900 text-zinc-800 dark:text-zinc-200 transition-colors duration-200">
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
-          <LanguageProvider>
-            <Header />
-            <main className="pt-16">
-              {children}
-            </main>
-          </LanguageProvider>
-        </ThemeProvider>
+    <html lang="pt-BR" className="h-full">
+      <body className="h-full flex items-center justify-center">
+        <h1 className="text-2xl">Em construção...</h1>
       </body>
     </html>
   );
